1eafa2309325d5b6b6b76f78e9bb98416f858512
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-04-04 17:36:22 -0300

    Correção

--- src/infracao/Infracao.java
+++ src/infracao/Infracao.java
@@ -4,4 +4,24 @@ public class Infracao {
     private String descricao;
     private GravidadeMulta gravidadeMulta;
 
+    public Infracao(String descricao, GravidadeMulta gravidadeMulta) {
+        this.descricao = descricao;
+        this.gravidadeMulta = gravidadeMulta;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public GravidadeMulta getGravidadeMulta() {
+        return gravidadeMulta;
+    }
+
+    public void setGravidadeMulta(GravidadeMulta gravidadeMulta) {
+        this.gravidadeMulta = gravidadeMulta;
+    }
 }
