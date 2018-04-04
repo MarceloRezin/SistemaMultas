d63db0c1c88a3436df1af1c3ba90099c868c1e25
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-04-04 17:15:46 -0300

    Modelagem orgao

---
+++ src/orgao/Orgao.java
@@ -0,0 +1,50 @@
+package orgao;
+
+import commons.enuns.Estado;
+
+public class Orgao {
+
+    private String nome;
+    private String endereco;
+    private String cidade;
+    private Estado estado;
+
+    public Orgao(String nome, String endereco, String cidade, Estado estado) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
