95aa90b24d1ad32bef821365e2d8ec8f27fec97e
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-04-04 17:25:38 -0300

    Enum com a gravidade, valor e pontos na carteira das multas brasileiras

---
+++ src/commons/enuns/GravidadeMulta.java
@@ -0,0 +1,33 @@
+package commons.enuns;
+
+import java.math.BigDecimal;
+
+public enum GravidadeMulta {
+    LEVE("Leve", new BigDecimal(88.38), 3),
+    MEDIA("Média", new BigDecimal(130.16), 4),
+    GRAVE("Grave", new BigDecimal(195.23), 5),
+    GRAVISSIMA("Gravíssima", new BigDecimal(293.47), 7);
+
+
+    private String descricao;
+    private BigDecimal valor;
+    private int numeroPontos;
+
+    GravidadeMulta(String descricao, BigDecimal valor, int numeroPontos) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.numeroPontos = numeroPontos;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public int getNumeroPontos() {
+        return numeroPontos;
+    }
+}
