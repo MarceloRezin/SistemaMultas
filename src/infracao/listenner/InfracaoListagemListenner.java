47f41aec8acaf9672952a58211c250500c3cbee6
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-04-06 21:29:50 -0300

    Infracao

--- src/infracao/listenner/InfracaoListagemListenner.java
+++ src/infracao/listenner/InfracaoListagemListenner.java
@@ -1,12 +1,19 @@
 package infracao.listenner;
 
 import commons.listenners.ListagemListenner;
+import infracao.telas.TelaInfracao;
 
 import javax.swing.*;
 
 public class InfracaoListagemListenner extends ListagemListenner {
-    public InfracaoListagemListenner(JButton listar, JButton novo) {
+    private JDesktopPane jDesktopPane;
+    private TelaInfracao telaInfracao = new TelaInfracao();
+    private JInternalFrame frame;
+
+    public InfracaoListagemListenner(JButton listar, JButton novo, JDesktopPane jDesktopPane, JInternalFrame frame) {
         super(listar, novo);
+        this.jDesktopPane = jDesktopPane;
+        this.frame = frame;
     }
 
     @Override
@@ -16,6 +23,9 @@ public class InfracaoListagemListenner extends ListagemListenner {
 
     @Override
     public void eventoNovo() {
+        frame.dispose();
         System.out.println("Infracao -> Novo");
+        jDesktopPane.add(telaInfracao);
+        telaInfracao.setVisible(true);
     }
 }
