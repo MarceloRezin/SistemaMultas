d44966d3c457584ae89886dcd657027d41bccbd7
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-04-06 21:13:09 -0300

    Correcoes

--- src/principal/PrincipalTela.java
+++ src/principal/PrincipalTela.java
@@ -1,5 +1,6 @@
 package principal;
 
+import infracao.telas.TelaConsultaInfracao;
 import proprietario.Proprietario;
 import proprietario.telas.TelaConsultaProprietario;
 import proprietario.telas.TelaProprietario;
@@ -24,24 +25,26 @@ public class PrincipalTela extends JFrame {
 		JMenu mnCadastro = new JMenu("Cadastro");
 		menuBar.add(mnCadastro);
 
-
-		TelaProprietario telaProprietario = new TelaProprietario();
 		TelaConsultaProprietario telaConsultaProprietario = new TelaConsultaProprietario(desktopPane);
-
 		JMenuItem mntmProprietario = new JMenuItem("Proprietario");
 		mntmProprietario.addActionListener( e -> {
 			telaConsultaProprietario.setVisible(true);
 			desktopPane.add(telaConsultaProprietario);
 		});
 		mnCadastro.add(mntmProprietario);
-		
+
 		JMenuItem mntmVeiculo = new JMenuItem("Veiculo");
 		mnCadastro.add(mntmVeiculo);
 		
 		JMenuItem mntmOrgo = new JMenuItem("Org\u00E3o");
 		mnCadastro.add(mntmOrgo);
-		
+
+		TelaConsultaInfracao telaConsultaInfracao = new TelaConsultaInfracao(desktopPane);
 		JMenuItem mntmInfrao = new JMenuItem("Infra\u00E7\u00E3o");
+		mntmInfrao.addActionListener(e -> {
+			telaConsultaInfracao.setVisible(true);
+			desktopPane.add(telaConsultaInfracao);
+		});
 		mnCadastro.add(mntmInfrao);
 		
 		JMenuItem mntmMulta = new JMenuItem("Multa");
