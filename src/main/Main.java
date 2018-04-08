390b3e42c4bf142b49827e806afdba330abc0724
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-04-08 14:46:25 -0300

    Definido o tema do sistema operacional para a aplicação

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,20 @@
 package main;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import usuario.login.UsuarioLoginTela;
 
 public class Main {
 
     public static void main(String[] args) {
+    	
+    	try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}		
 
         new UsuarioLoginTela().setVisible(true);
     }
