168e29b6e52547986dbbf2faf9fceb9d4b853d72
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-05-06 13:48:07 -0300

    Atualização para persistencia

--- src/commons/cadastros/Cadastro.java
+++ src/commons/cadastros/Cadastro.java
@@ -1,5 +1,10 @@
 package commons.cadastros;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public abstract class Cadastro {
 
 	private Integer id;
@@ -12,5 +17,62 @@ public abstract class Cadastro {
 		this.id = id;
 	}
 
+	public boolean isNovo(){
+	    return id == null;
+    }
+
+    /**
+     * Utilizado no list da tela de listagem
+     * @return
+     */
 	public abstract String toString();
+
+	/**
+	 * Retorna o nome da tabela utilizada no banco
+	 * @return
+	 */
+	public abstract String getNomeTabela();
+
+	/**
+	 * Retornar todas as colunas que serão gravadas no insert com o padrão: coluna1,coluna2,coluna3
+     * Não é necessário informar o id
+	 * @return
+	 */
+	public abstract String getColunas();
+
+
+	/**
+	 * Retorna a substituicao das colunas por ?,?,?
+	 * @return
+	 */
+	public String getColunasSubstituicao(){
+		String[] lista = getColunas().split(",");
+
+		String result = "";
+
+		for(int i=0; i<lista.length; i++){
+			if(i == lista.length -1){
+				result += "?";
+			}else{
+				result += "?,";
+			}
+		}
+
+		return result;
+	}
+
+    /**
+     * Define os valores que serao inseridos nas colunas
+     * @param stmt
+     * @throws SQLException
+     */
+	public abstract void setStatements(PreparedStatement stmt) throws SQLException;
+
+    /**
+     * Converte result da consulta em uma lista de objetos
+     * @param rs
+     * @return
+     * @throws SQLException
+     */
+	public abstract List resultSetToList(ResultSet rs) throws SQLException;
 }
