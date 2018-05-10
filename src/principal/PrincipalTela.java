f8292e0e3ee1f65e0962c1f0ab5cafd6049b679d
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-05-10 18:41:44 -0300

    Comentado outras funções para apresentação

--- src/principal/PrincipalTela.java
+++ src/principal/PrincipalTela.java
@@ -28,29 +28,29 @@ public class PrincipalTela extends JFrame {
 		JMenu mnCadastro = new JMenu("Cadastro");
 		menuBar.add(mnCadastro);
 
-		JMenuItem mntmProprietario = new JMenuItem("Proprietario");
-		mntmProprietario.addActionListener( e -> {
-			ProprietarioConsultaTela proprietarioConsultaTela = new ProprietarioConsultaTela(desktopPane);
-			proprietarioConsultaTela.setVisible(true);
-			desktopPane.add(proprietarioConsultaTela);
-		});
-		mnCadastro.add(mntmProprietario);
-
-		JMenuItem mntmVeiculo = new JMenuItem("Veiculo");
-		mntmVeiculo.addActionListener( e -> {
-			VeiculoConsultaTela veiculoConsultaTela = new VeiculoConsultaTela(desktopPane);
-			veiculoConsultaTela.setVisible(true);
-			desktopPane.add(veiculoConsultaTela);
-		});
-		mnCadastro.add(mntmVeiculo);
-
-		JMenuItem mntmOrgo = new JMenuItem("Orgão");
-		mntmOrgo.addActionListener( e -> {
-			OrgaoConsultaTela orgaoConsultaTela = new OrgaoConsultaTela(desktopPane);
-			orgaoConsultaTela.setVisible(true);
-			desktopPane.add(orgaoConsultaTela);
-		});
-		mnCadastro.add(mntmOrgo);
+//		JMenuItem mntmProprietario = new JMenuItem("Proprietario");
+//		mntmProprietario.addActionListener( e -> {
+//			ProprietarioConsultaTela proprietarioConsultaTela = new ProprietarioConsultaTela(desktopPane);
+//			proprietarioConsultaTela.setVisible(true);
+//			desktopPane.add(proprietarioConsultaTela);
+//		});
+//		mnCadastro.add(mntmProprietario);
+//
+//		JMenuItem mntmVeiculo = new JMenuItem("Veiculo");
+//		mntmVeiculo.addActionListener( e -> {
+//			VeiculoConsultaTela veiculoConsultaTela = new VeiculoConsultaTela(desktopPane);
+//			veiculoConsultaTela.setVisible(true);
+//			desktopPane.add(veiculoConsultaTela);
+//		});
+//		mnCadastro.add(mntmVeiculo);
+//
+//		JMenuItem mntmOrgo = new JMenuItem("Orgão");
+//		mntmOrgo.addActionListener( e -> {
+//			OrgaoConsultaTela orgaoConsultaTela = new OrgaoConsultaTela(desktopPane);
+//			orgaoConsultaTela.setVisible(true);
+//			desktopPane.add(orgaoConsultaTela);
+//		});
+//		mnCadastro.add(mntmOrgo);
 
 		JMenuItem mntmGravidadeMulta = new JMenuItem("Gravidade das Multas");
         mntmGravidadeMulta.addActionListener(e -> {
@@ -60,31 +60,31 @@ public class PrincipalTela extends JFrame {
 		});
 		mnCadastro.add(mntmGravidadeMulta);
 
-		JMenuItem mntmInfracao = new JMenuItem("Infração");
-		mntmInfracao.addActionListener(e -> {
-			InfracaoConsultaTela telaConsultaInfracao = new InfracaoConsultaTela(desktopPane);
-			telaConsultaInfracao.setVisible(true);
-			desktopPane.add(telaConsultaInfracao);
-		});
-		mnCadastro.add(mntmInfracao);
-
-		JMenuItem mntmMulta = new JMenuItem("Multa");
-		mntmMulta.addActionListener(e -> {
-			MultaConsultaTela telaConsultaMulta = new MultaConsultaTela(desktopPane);
-		    telaConsultaMulta.setVisible(true);
-		    desktopPane.add(telaConsultaMulta);
-        });
-		mnCadastro.add(mntmMulta);
-		
-		JMenu mnUsuario = new JMenu("Usuarios");
-		JMenuItem gerenciarUsuarios = new JMenuItem("Gerenciar Usuários");
-		gerenciarUsuarios.addActionListener( e -> {
-			UsuarioConsultaTela usuarioConsultaTela = new UsuarioConsultaTela(desktopPane);
-			usuarioConsultaTela.setVisible(true);
-			desktopPane.add(usuarioConsultaTela);
-		});
-		mnUsuario.add(gerenciarUsuarios);
-		menuBar.add(mnUsuario);
+//		JMenuItem mntmInfracao = new JMenuItem("Infração");
+//		mntmInfracao.addActionListener(e -> {
+//			InfracaoConsultaTela telaConsultaInfracao = new InfracaoConsultaTela(desktopPane);
+//			telaConsultaInfracao.setVisible(true);
+//			desktopPane.add(telaConsultaInfracao);
+//		});
+//		mnCadastro.add(mntmInfracao);
+//
+//		JMenuItem mntmMulta = new JMenuItem("Multa");
+//		mntmMulta.addActionListener(e -> {
+//			MultaConsultaTela telaConsultaMulta = new MultaConsultaTela(desktopPane);
+//		    telaConsultaMulta.setVisible(true);
+//		    desktopPane.add(telaConsultaMulta);
+//        });
+//		mnCadastro.add(mntmMulta);
+//
+//		JMenu mnUsuario = new JMenu("Usuarios");
+//		JMenuItem gerenciarUsuarios = new JMenuItem("Gerenciar Usuários");
+//		gerenciarUsuarios.addActionListener( e -> {
+//			UsuarioConsultaTela usuarioConsultaTela = new UsuarioConsultaTela(desktopPane);
+//			usuarioConsultaTela.setVisible(true);
+//			desktopPane.add(usuarioConsultaTela);
+//		});
+//		mnUsuario.add(gerenciarUsuarios);
+//		menuBar.add(mnUsuario);
 
 
 		JMenu mnSobre = new JMenu("Sobre");
@@ -92,7 +92,7 @@ public class PrincipalTela extends JFrame {
 		mnSobre.add(mntSobre);
 		menuBar.add(mnSobre);
 		mntSobre.addActionListener(e->{
-			JOptionPane.showMessageDialog(null,"Desenvolvedores: Lucas Claro e Marcelo Rezin \n Versão: 0.1","Sobre",JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null,"Desenvolvedores: Lucas Claro e Marcelo Rezin \n Versão: 0.2","Sobre",JOptionPane.INFORMATION_MESSAGE);
 		});
 
         addWindowListener(new WindowAdapter() {
