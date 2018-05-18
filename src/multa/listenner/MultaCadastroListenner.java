7fc69b6bee8efc5e38f4a1f7a7139400ec213eb6
Lucas <lucasclaro10@hotmail.com>
2018-05-17 21:44:50 -0300

    Implementação Função log

--- src/multa/listenner/MultaCadastroListenner.java
+++ src/multa/listenner/MultaCadastroListenner.java
@@ -2,6 +2,7 @@ package multa.listenner;
 
 import javax.swing.*;
 import commons.listenners.CadastroListenner;
+import log.Logger;
 
 public class MultaCadastroListenner extends CadastroListenner {
 
@@ -11,12 +12,16 @@ public class MultaCadastroListenner extends CadastroListenner {
 
     @Override
     public void eventoSalvar() {
+
         System.out.println("Multa -> Salvar");
+        Logger.log("Multa, Salvou");
+
     }
 
     @Override
     public void eventoExcluir() {
         System.out.println("Multa -> Excluir");
+        Logger.log("Multa, Excluiu");
     }
 
     @Override
