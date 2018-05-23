3a7ac557a56d0778b14e9715d4e42a2be56c828a
Marcelo Rezin <marcelo.rezin@gmail.com>
2018-05-23 18:58:03 -0300

    Alteração

--- src/exception/SistemaMultasException.java
+++ src/exception/SistemaMultasException.java
@@ -3,7 +3,7 @@ package exception;
 public final class SistemaMultasException extends Exception{
 
     public SistemaMultasException() {
-        super("Ocorreu um erro desconhecido!");
+        super("Ocorreu um erro!");
     }
     public SistemaMultasException(String msg) {
         super(msg);
